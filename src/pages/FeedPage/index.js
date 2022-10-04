390e9ac1c06b75c04581e27aacf50784869d0a67
AnaPaulaSil <anapaulasilveira52783@gmail.com>
2022-10-04 13:05:24 -0300

    nova biblioteca instalada, alterações em algumas paginas

--- src/pages/FeedPage/index.js
+++ src/pages/FeedPage/index.js
@@ -1,6 +1,18 @@
+import { Card } from "react-bootstrap"
+import {api} from "../../api/api"
+
 function FeedPage() {
+    async function cardsUsers() {
+        try {
+            const response = await api.get(`/users/all`)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     return (
         <>
+        <h1>feed</h1>
         </>
     )
 }
