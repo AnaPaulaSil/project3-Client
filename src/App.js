b29dabb89f02405bffe71dfbc2f54c305b875519
AndreaNaomi <kutaninaomi@gmail.com>
2022-10-04 10:15:22 -0300

    HomePage e Signup inciado

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+
+import HomePage from "./pages/HomePage";
+import SignUpPage from "./pages/SignUpPage";
+
+import { AuthContextComponent } from"./context/authContext";
 
 function App() {
   return (
     <div className="App">
-    
+      <AuthContextComponent>
+
+      <Routes>
+        <Route path="/" element={<HomePage/>}></Route>
+        <Route path="sign-up" element={<SignUpPage/>}></Route>
+        {/* <Route
+            path="/users/:idUser"
+            element={<ProtectRoute Component={UsersPage} />}
+          /> */}
+      </Routes>
+
+      </AuthContextComponent>
     </div>
   );
 }
