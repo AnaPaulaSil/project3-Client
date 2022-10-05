d885238f8ed96b51408f189cdf26a3aa2a5c38d1
AnaPaulaSil <anapaulasilveira52783@gmail.com>
2022-10-05 08:56:36 -0300

    profile page incompleta

--- src/pages/ProfileDetailPage/index.js
+++ src/pages/ProfileDetailPage/index.js
@@ -1,6 +1,7 @@
 function ProfileDetailPage() {
     return(
         <>
+        
         </>
     )
 } 
--- src/pages/ProfilePage/index.js
+++ src/pages/ProfilePage/index.js
@@ -1,7 +1,9 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import { api } from "../../api/api";
 import Navbarr from "../../components/Navbar";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../context/authContext";
+
 
 function ProfilePage() {
   const [form, setForm] = useState({
@@ -10,38 +12,102 @@ function ProfilePage() {
     like: [],
   });
 
+  const { loggedInUser } = useContext(AuthContext);
+
+  const [users, setUsers] = useState([]);
+  const [posts, setPosts] = useState([]);
+  const [editPost, setEditPost] = useState([]);
+  const [deletePost, setDeletePost] = useState([]);
+ 
+
   function handleChange(e) {
-    setForm({...form, [e.target.name]: e.target.value})
+    setForm({ ...form, [e.target.name]: e.target.value });
   }
 
   async function handleSubmit(e) {
-    e.preventDefault()
+    e.preventDefault();
     try {
-        await api.post("/posts/create-post")
+      await api.post("/posts/create-post");
     } catch (error) {
-        console.log(error)
+      console.log(error);
     }
   }
 
-  //edit post (btn)
-  //delet post (btn)
+  async function EditProfile() {
+      try {
+          const response = await api.put(`/users/edit`);
 
+          setUsers(response.data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    async function AllPosts() {
+        try {
+            const allPosts = await api.get(`posts/all-posts`)
+            setPosts(allPosts.data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+  //edit post (btn)
+    async function EditPost() {
+        try {
+            const editPost = await api.put(`posts/edit-post/:idPost`)
+            setEditPost(editPost.data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+  //delet post (btn)
+    async function deletePost() {
+        try {
+            const deletePost = await api.delete(`deleted-post/:idPost`)
+            setDeletePost(deletePost.data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
   return (
     <>
-    <Navbarr/>
+      <Navbarr />
+      
+      {users.filter(()=>{
+        if (loggedInUser._id == users._id){
+            return
+        }
+      })
+      .map((user) => {
+        return (
+          <>
+            <img
+              src={user.profilePic}
+              alt="user photo"
+              style={{ width: "140px" }}
+            />
+            <h1>{user.username}</h1>
+            <p>{user.bio}</p>
+            <button onChange={EditProfile}>Editar Perfil</button>
+          </>
+        );
+      })}
 
-    <Link to="/chat">chat</Link>
+      {/* <Link to="/chat">chat</Link> */}
 
-    <form>
-      <label>Faça um post:</label>
-      <textarea
-        placeholder="Digite aqui..."
-        name="content"
-        type="text"
-        value={form.content}
-        onChange={handleChange}
-      />
-    </form>
+      <form>
+        <label>Faça um post:</label>
+        <textarea
+          placeholder="Digite aqui..."
+          name="content"
+          type="text"
+          value={form.content}
+          onChange={handleChange}
+        />
+        <button onChange={handleSubmit}>Postar</button>
+      </form>
     </>
   );
 }
