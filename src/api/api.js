a3faad7e83ed260eb02fb2c9450981015a89bbd4
AnaPaulaSil <anapaulasilveira52783@gmail.com>
2022-10-06 18:25:42 -0300

    final push

--- src/api/api.js
+++ src/api/api.js
@@ -2,15 +2,15 @@ import axios from "axios";
 
 const apiURLs = {
   development: "http://localhost:4000",
-//   production: "",
+  production:
+    "mongodb+srv://andreank:andreank@conectabancodedados.1ws7kgg.mongodb.net/?retryWrites=true&w=majority",
 };
 
 const api = axios.create({ baseURL: apiURLs[process.env.NODE_ENV] });
 
-
 api.interceptors.request.use((config) => {
-  const loggedInUserJSON = localStorage.getItem("loggedInUser"); 
-  const parseLoggedInUser = JSON.parse(loggedInUserJSON || '""'); 
+  const loggedInUserJSON = localStorage.getItem("loggedInUser");
+  const parseLoggedInUser = JSON.parse(loggedInUserJSON || '""');
 
   if (parseLoggedInUser.token) {
     config.headers = { Authorization: `Bearer ${parseLoggedInUser.token}` };
